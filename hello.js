2a6a62a5610b334bde6d91b4e2336aaf8bd94bd6
GoddamSam <155828538+GoddamSam@users.noreply.github.com>
2024-01-06 19:44:24 +0530

    Added files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+var phone=9163969526;
+var name=Number.Nan;
+// if(isNaN(name))
+// {
+//     console.log("Enter valid phone");
+// }
+// else
+// {
+//     console.log(name);
+// }
+console.log(typeof(name));
